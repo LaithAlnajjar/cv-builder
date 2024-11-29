ae4129e4bda1d8b0651e942db237df05431b5dc4
Laith Alnajjar <laithalnajjar2004@gmail.com>
2024-11-29 15:27:25 +0300

    style: create styling for CV Preview

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,9 +14,56 @@ function App() {
     phoneNumber:'0994124912', 
     email:'laithalnajjar2004@gmail.com'
   })
-  const [education, setEducation] = useState ([])
-  const [experience, setExperience] = useState([])
-  const [skills, setSkills] = useState([])
+  const [education, setEducation] = useState ([
+    {
+      school:'The Hashemite University',
+      startDate:'26/10/2022',
+      graduationDate:'9/5/2026',
+      degree:'Bachelors of Computer Science',
+      key:0
+    },
+    {
+      school:'The Hashemite University',
+      startDate:'26/10/2022',
+      graduationDate:'9/5/2026',
+      degree:'Bachelors of Computer Science',
+      key:0
+    }
+  ])
+  const [experience, setExperience] = useState([
+    {
+      companyName:' Tech Innovators Inc.',
+      job:'Software Develoepr',
+      startDate:'January 2022',
+      endDate:'October 2024',
+      desc:`Designed and implemented scalable web applications, improving user engagement by 25%. \n
+            Collaborated with cross-functional teams to deliver projects on time and within budget. \n
+            Optimized backend APIs, reducing response times by 40%. \n
+            Mentored junior developers and conducted code reviews to ensure high-quality standards. \n
+            Led the integration of modern frameworks, enhancing overall system performance.`
+
+    },
+    {
+      job: "Project Manager",
+      companyName: "Visionary Solutions Ltd.",
+      startDate: "March 2020",
+      endDate: "August 2023",
+      desc: [
+        "Led a team of 10 in delivering software solutions, achieving a 95% client satisfaction rate.",
+        "Managed project timelines, budgets, and resources, ensuring on-time and within-budget delivery.",
+        "Implemented Agile methodologies to improve team collaboration and productivity by 30%.",
+        "Coordinated with stakeholders to define project requirements and deliverables.",
+        "Developed detailed project documentation and presented progress reports to senior management.",
+      ],
+      key:-1
+    }
+  ])
+  const [skills, setSkills] = useState([
+    {
+      title:'Technical',
+      skillList:'Python, Javascript, Databases'
+    }
+  ])
 
   return (
     <>
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -3,42 +3,62 @@ import '../styles/Preview.css';
 export default function Preview({ name, address, phoneNumber, email, education, experience, skills }) {
     
     return (
-        <div className="preview">
+        <div className="preview-container">
 
-            <div className="personal-information">
+            <div className="cv-personal-information">
                 <h1 className="name">{name}</h1>
-                <h2 className="address">{address}</h2>
-                <h2 className="phoneNumber">{phoneNumber}</h2>
-                <h2 className="email">{email}</h2>
+                <div className="sub-heading">
+                    <h2 className="address">{address}</h2>
+                    <div className='dot'>.</div>
+                    <h2 className="phoneNumber">{phoneNumber}</h2>
+                    <div className='dot'>.</div>
+                    <h2 className="email">{email}</h2>
+                </div>
             </div>
 
             <div className="education">
+                <span className="section-title">Education</span>
+                <hr/>
                 {education.map(item => {
                    return (<div className="education-item" key={item.key}>
-                        <div> {item.school} </div>
-                        <div> {item.startDate} </div>
-                        <div> {item.graduationDate} </div>
-                        <div> {item.degree}</div>
+                        <div className="education-nd">
+                            <div className='school-name'> {item.school} </div>
+                            <div className="education-date">
+                                <div> {item.startDate} </div>
+                                -
+                                <div> {item.graduationDate} </div>
+                            </div>
+                        </div>
+                        <div className='degree'> {item.degree}</div>
                     </div>)
                 } )}
             </div>
 
             <div className="experience">
+                <div className="section-title">Experience</div>
+                <hr/>
                 {experience.map(item => {
                    return (<div className="experience-item" key={item.key}>
-                        <div> {item.companyName} </div>
-                        <div> {item.job} </div>
-                        <div> {item.startDate} </div>
-                        <div> {item.endDate} </div>
+                        <div className="job-section">
+                            <div className='job-title'> {item.job} </div>
+                            <div className="job-date">
+                                <div> {item.startDate} </div>
+                                -
+                                <div> {item.endDate} </div>
+                            </div>
+                        </div>
+                        <div className='company-name'> {item.companyName} </div>
                         <div> {item.desc}</div>
                     </div>)
                 } )}
             </div>
 
             <div className="skills">
+                <div className="section-title">Skills</div>
+                <hr/>
                 {skills.map(item => {
                    return (<div className="skills-item" key={item.key}>
-                        <div> {item.title} </div>
+                        <div className='skill-title'> {item.title}: </div>
                         <div> {item.skillList} </div>
                     </div>)
                 } )}
